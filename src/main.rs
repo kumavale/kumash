ee5c9ae042187db5f4db461f8f45ee266e9a904c
kumavale <kumavale.hyperion@gmail.com>
2023-11-11 17:47:53 +0900

    refactor: integration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,40 @@
-use kumash::shell::Shell;
+use anyhow::anyhow;
+use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     env_logger::init();
 
-    let shell = Shell::default();
-    shell.start();
+    loop {
+        // Prompt
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        // Read line
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+
+        // Parse input line
+        let tokens = input_line.split_whitespace().collect::<Vec<&str>>();
+        if tokens.is_empty() {
+            continue;
+        }
+
+        // Execute
+        if let Err(e) = execute(tokens) {
+            log::error!("fatal error: {e}");
+        }
+    }
+}
+
+fn execute(tokens: Vec<&str>) -> anyhow::Result<i32> {
+    if tokens[0] == "exit" {
+        println!("(^-^)/~~");
+        std::process::exit(0);
+    }
+
+    let bin = tokens[0];
+    let args = if tokens.len() == 1 { &[] } else { &tokens[1..] };
+    let status = Command::new(bin).args(args).status()?;
+    status.code().ok_or(anyhow!("Process terminated by signal"))
 }
--- src/parser.rs
+++
@@ -1,3 +0,0 @@
-pub fn parse_input_line(input_line: &str) -> Vec<&str> {
-    input_line.split_whitespace().collect()
-}
