24ae51396c71f0f2de5a605df42126398e7ad812
kumavale <kumavale.hyperion@gmail.com>
2023-11-11 20:48:37 +0900

    feat: pipe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use anyhow::Context;
 use std::io::{self, Write};
-use std::process::Command;
+use std::process::{Child, Command, Stdio};
 
 fn main() {
     env_logger::init();
@@ -14,32 +15,57 @@ fn main() {
         io::stdin().read_line(&mut input_line).unwrap();
 
         // Parse input line
-        let tokens = input_line.split_whitespace();
+        let mut commands = input_line.split(" | ").peekable();
 
-        // Execute
-        if let Err(e) = execute(tokens) {
-            log::error!("fatal error: {e}");
+        let mut previous_process = None;
+        while let Some(command) = commands.next() {
+            let tokens = command.split_whitespace();
+
+            // Execute
+            let stdout = commands.peek().map_or(Stdio::inherit(), |_| Stdio::piped());
+            match execute(tokens, previous_process, stdout) {
+                Ok(child) => previous_process = Some(child),
+                Err(e) => {
+                    log::error!("kumash error: {e}");
+                    previous_process = None;
+                    break;
+                }
+            }
+        }
+
+        if let Some(final_process) = previous_process {
+            match final_process.wait_with_output() {
+                Ok(output) => io::stdout().write_all(&output.stdout).unwrap(),
+                Err(e) => log::error!("kumash error: {e}"),
+            }
         }
     }
 }
 
-fn execute<'a, I>(mut tokens: I) -> anyhow::Result<()>
+fn execute<'a, I>(
+    mut tokens: I,
+    previous_process: Option<Child>,
+    stdout: Stdio,
+) -> anyhow::Result<Child>
 where
     I: Iterator<Item = &'a str>,
 {
-    let command = match tokens.next() {
-        Some(command) => command,
-        None => return Ok(()),
-    };
-
+    let command = tokens.next().context("no command entered")?;
     match command {
         "exit" => {
             println!("(^-^)/~~");
             std::process::exit(0);
         }
         _ => {
-            Command::new(command).args(tokens).spawn()?.wait()?;
+            let stdin = previous_process.map_or(Stdio::inherit(), |child: Child| {
+                Stdio::from(child.stdout.unwrap())
+            });
+            let child = Command::new(command)
+                .args(tokens)
+                .stdin(stdin)
+                .stdout(stdout)
+                .spawn()?;
+            Ok(child)
         }
     }
-    Ok(())
 }
